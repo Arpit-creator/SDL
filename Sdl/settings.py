25794ef5e4ef94c32d6df11815c7b51b48e68c1e
Arpit Chakladar <54011232+Arpit-creator@users.noreply.github.com>
2019-11-09 21:49:22 +0530

    Update settings.py

--- Sdl/settings.py
+++ Sdl/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1k_bu0vryte5-xu^^&@8ont^h*bo^243ug8jt#+42wax)w4cg*'
+SECRET_KEY = '(key here)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
